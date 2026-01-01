73528275d700b5a43b3a1575d356cf824e405635
Ritesh Patel <patelritesh8508@gmail.com>
2026-01-01 18:43:25 +0530

    setup the dark mode

--- task2/src/component/DarkMode/DarkMode.jsx
+++ task2/src/component/DarkMode/DarkMode.jsx
@@ -4,6 +4,7 @@ import "./DarkMode.css";
 const DarkMode = () => {
   const [isDark, setIsDark] = useState(false);
 
+  // useEffect told us that is kaam ko render ke baad chalana hai.
   useEffect(() => {
     if (isDark) {
       document.body.classList.add("dark");
@@ -13,15 +14,21 @@ const DarkMode = () => {
   }, [isDark]);
 
   return (
-    <div className="toggle-wrapper">
-      <input
-        type="checkbox"
-        id="switch"
-        checked={isDark}
-        onChange={() => setIsDark(!isDark)}
-        hidden
-      />
-      <label htmlFor="switch" className="switch"></label>
+
+    <div className="main">
+        <div className="toggle-wrapper">
+              <input
+                type="checkbox"
+                id="switch"
+                checked={isDark}
+                onChange={() => setIsDark(isDark => !isDark)} // best practice
+                hidden
+              />
+              <label htmlFor="switch" className="switch"></label>
+        </div>
+        
+        <div className="num-counter">
+        </div>
     </div>
   );
 };
